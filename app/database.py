c9df630ecd7df340bb414044f7a5766af9bcaa4e
Sibeil <sibeil@yahoo.ie>
2026-04-02 00:21:25 +0100

    changing database connection to use environment  variable for security reasons

--- app/database.py
+++ app/database.py
@@ -1,5 +1,7 @@
+import os
+
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://admin:sibeil123@cluster0.p3ovyvo.mongodb.net/?appName=Cluster0")
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["products_db"]
 collection = db["products"]
